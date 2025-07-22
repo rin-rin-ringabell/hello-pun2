d04898f974037ad4e1d9f9b21a68f0b206e81772
rin-rin-ringabell <is4u2000@gmail.com>
2025-07-22 13:03:27 +0900

    Update RoomMain.cs

--- Assets/2.Scripts/RoomMain.cs
+++ Assets/2.Scripts/RoomMain.cs
@@ -18,12 +18,18 @@ public class RoomMain : MonoBehaviourPunCallbacks
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         Debug.Log($"[RoomyMain] 다른 플레이어가 룸에 입장 했습니다. : {newPlayer}");
+        player2Nickname.text = newPlayer.NickName;
     }
     public override void OnJoinedRoom()
     {
         Debug.Log("[RoomMain] OnJoinedRoom");        
         Debug.Log($"[RoomMain] {PhotonNetwork.CurrentRoom.Name}방에 입장했습니다.");
         Debug.Log($"{PhotonNetwork.LocalPlayer.NickName}가 방장인가? : {PhotonNetwork.IsMasterClient}");
+
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            player1Nickname.text = PhotonNetwork.MasterClient.NickName;
+        }
     }
 
     public void Init()
